b25118c42c8915c9f2e1aec099f775d775147f7d
Nick <lis@chaikovskie.com>
2021-09-07 23:21:50 +0300

    feat: add allowed extensions to prevent useless button show

--- extension/src/utils/setupPreviewButtons.ts
+++ extension/src/utils/setupPreviewButtons.ts
@@ -4,10 +4,15 @@ import PreviewButton from "@/ui/PreviewButton.vue";
 import { createApp } from "vue";
 
 export function setupPreviewButtons() {
+  const allowedExtensions = ["pdf", "jpg", "png", "jpeg"];
   const pageBlocks = parsePage();
 
   pageBlocks.forEach((block) => {
     block.files.forEach((file) => {
+      if (!allowedExtensions.includes(file.extension)) {
+        return;
+      }
+
       const fileId = generateId();
       const mountElement = document.createElement("div");
       mountElement.setAttribute("id", fileId);
