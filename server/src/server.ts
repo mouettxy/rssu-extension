c4dd78b1fac3305770b95a0dc1c273f678df2473
Nick <lis@chaikovskie.com>
2021-11-24 00:09:53 +0300

    fix(server): cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 import fastify, { FastifyInstance } from 'fastify';
 import cors from 'fastify-cors';
-import helmet from 'fastify-helmet';
+// import helmet from 'fastify-helmet';
 
 import config from './config';
 import uploadFile from './routes/uploadFile';
@@ -9,11 +9,8 @@ import sendFile from './routes/sendFile';
 export const configureServer = (): FastifyInstance => {
   const server = fastify({ logger: config.logger, genReqId: config.genReqId });
 
-  server.register(cors as any, {
-    origin: 'https://sdo.rgsu.net',
-    methods: ['GET', 'POST'],
-  });
-  server.register(helmet);
+  server.register(cors as any);
+  // server.register(helmet);
 
   server.register(uploadFile, { prefix: '/v1' });
   server.register(sendFile, { prefix: '/v1' });
